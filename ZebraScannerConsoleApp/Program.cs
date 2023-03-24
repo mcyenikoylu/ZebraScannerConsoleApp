f58d6401900ff5311b0f4af2c5b8517718431fd7
Mehmet Cenk Yeniköylü <mcy@dotnetmcy.net>
2023-03-24 06:59:45 +0300

    GetScanners

--- ZebraScannerConsoleApp/Program.cs
+++ ZebraScannerConsoleApp/Program.cs
@@ -19,15 +19,27 @@ namespace ZebraScannerConsoleApp
             scannerTypes[0] = 1; // 1 for all scanner types
             short numberOfScannerTypes = 1; // Size of the scannerTypes array
             int status; // Extended API return code
+
             cCoreScannerClass.Open(0, scannerTypes, numberOfScannerTypes, out status);
-            if (status == 0)
-            {
-                Console.WriteLine("CoreScanner API: Open Successful");
-            }
-            else
-            {
-                Console.WriteLine("CoreScanner API: Open Failed");
-            }
+
+            //if (status == 0)
+            //{
+            //    Console.WriteLine("CoreScanner API: Open Successful");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("CoreScanner API: Open Failed");
+            //}
+
+            // Lets list down all the scanners connected to the host
+            short numberOfScanners; // Number of scanners expect to be used
+            int[] connectedScannerIDList = new int[255];
+            // List of scanner IDs to be returned
+            string outXML; //Scanner details output
+            cCoreScannerClass.GetScanners(out numberOfScanners, connectedScannerIDList, out outXML, out status);
+
+            Console.WriteLine(outXML);
+
         }
     }
 }
